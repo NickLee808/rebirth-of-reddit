e411658738fa80951f458e97d27a55d426fc622b
Nick Lee <nicklee808@gmail.com>
2016-12-17 20:20:38 -1000

    DOM MVP setup

--- public/js/app.js
+++ public/js/app.js
@@ -3,10 +3,12 @@
 function reqListener () {
   let objParse = JSON.parse(this.responseText);
   for (let i = 0; i<objParse.data.children.length; i++) {
-    console.log(objParse.data.children[i].data.url);
-    var element = document.createElement('img');
-    element.setAttribute('src', objParse.data.children[i].data.url);
-    document.getElementById('pics').appendChild(element);
+    let pic = document.createElement('img');
+    let link = document.createElement('span');
+    link.setAttribute('id', `link${i}`);
+    pic.setAttribute('src', objParse.data.children[i].data.url);
+    document.getElementById('gallery').appendChild(link);
+    document.getElementById(`link${i}`).appendChild(pic);
   }
 }
 
