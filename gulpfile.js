5c86198175fc6dc5de2ebd858630d1868c740029
Nick Lee <nicklee808@gmail.com>
2016-12-17 17:05:36 -1000

    workflow setup v2.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,31 @@
-/*jshint esversion:6*/
-
 const gulp = require('gulp');
 const scss = require('gulp-sass');
-const browserSync = require('browser-sync');
+const browserSync = require('browser-sync').create();
 
 gulp.task('scss', () => {
   return gulp.src('./scss/*.scss')
-  .pipe(scss())
-  .pipe(gulp.dest('./public/css'));
+    .pipe(scss())
+    .pipe(gulp.dest('./public/css'))
+})
+
+// for the browser-sync people
+gulp.task('browserSync', () => {
+  browserSync.init(['./public'], {
+    server: {
+      baseDir: './public'
+    }
+  })
+})
+
+// for the gulp-connect people
+// const connect = require('gulp-connect')
+gulp.task('livereload', function (){
+  gulp.src('./public/**/*')
+  .pipe(connect.reload());
 });
+
+gulp.task('watch', ['browserSync'], () => {
+  gulp.watch('./scss/**/*.scss', ['scss'])
+})
+
+gulp.task('default', ['scss', 'watch']);
