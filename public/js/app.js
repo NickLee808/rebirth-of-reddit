f482c7ab8d6c22ca6358f7f58c7305cd39738be7
Nick Lee <nicklee808@gmail.com>
2016-12-21 18:10:44 -1000

    youtube added

--- public/js/app.js
+++ public/js/app.js
@@ -9,21 +9,35 @@ function linkLoader () {
   let objParse = JSON.parse(this.responseText);
   let arrayOfPosts = objParse.data.children;
   for (let i = 0; i < arrayOfPosts.length; i++) {
-    if (formatAuthenticator(arrayOfPosts[i].data.url)){
-      let pic = document.createElement('img');
+    if (formatAuthenticator(arrayOfPosts[i].data.url)) {
       let link = document.createElement('span');
       link.setAttribute('id', `link${i}`);
-      pic.setAttribute('src', objParse.data.children[i].data.url.split('&amp;').join('&').split('gifv').join('gif'));
       document.getElementById('gallery').appendChild(link);
-      link.appendChild(pic);
+      if (arrayOfPosts[i].data.url.indexOf('youtu') > -1){
+        let vid = document.createElement('iframe');
+        vid.setAttribute('src', youTuber(arrayOfPosts[i].data.url));
+        link.appendChild(vid);
+      }else{
+        let pic = document.createElement('img');
+        pic.setAttribute('src', objParse.data.children[i].data.url.split('&amp;').join('&').split('gifv').join('gif'));
+        link.appendChild(pic);
+      }
     }else{
       console.log('broken link: ' + arrayOfPosts[i].data.url);
     }
   }
 }
 
+function youTuber (url) {
+  if (url.indexOf('youtube.com/watch?v=') > -1){
+    return 'http://youtube.com/embed/' + url.split('youtube.com/watch?v=')[1] + '?rel=0';
+  }else if(url.indexOf('youtu.be') > -1){
+    return 'http://youtube.com/embed/' + url.split('youtu.be/')[1] + '?rel=0';
+  }
+}
+
 function formatAuthenticator (url) {
-  let whiteList = ['.jpg', '.jpeg', '.gif', '.png', 'reddituploads'];
+  let whiteList = ['.jpg', '.jpeg', '.gif', '.png', 'reddituploads', 'youtu'];
   for (let i = 0; i < whiteList.length; i++){
     if (url.indexOf(whiteList[i]) > -1){
       return true;
