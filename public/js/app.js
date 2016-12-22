1a6b13855bd7b99f00fccefa8d712048aa5e4e35
Nick Lee <nicklee808@gmail.com>
2016-12-21 17:07:19 -1000

    file format check, begin youtube container

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,37 @@
 //jshint esversion:6
 
-function reqListener () {
+var oReq = new XMLHttpRequest();
+oReq.addEventListener('load', linkLoader);
+oReq.open('GET', 'https://www.reddit.com/r/birdswitharms.json');
+oReq.send();
+
+function linkLoader () {
   let objParse = JSON.parse(this.responseText);
-  for (let i = 0; i<objParse.data.children.length; i++) {
-    let pic = document.createElement('img');
-    let link = document.createElement('span');
-    link.setAttribute('id', `link${i}`);
-    pic.setAttribute('src', objParse.data.children[i].data.url);
-    document.getElementById('gallery').appendChild(link);
-    link.appendChild(pic);
+  let arrayOfPosts = objParse.data.children;
+  for (let i = 0; i < arrayOfPosts.length; i++) {
+    if (formatAuthenticator(arrayOfPosts[i].data.url)){
+      let pic = document.createElement('img');
+      let link = document.createElement('span');
+      link.setAttribute('id', `link${i}`);
+      pic.setAttribute('src', objParse.data.children[i].data.url.split('&amp;').join('&').split('gifv').join('gif'));
+      document.getElementById('gallery').appendChild(link);
+      link.appendChild(pic);
+    }else{
+      console.log('broken link: ' + arrayOfPosts[i].data.url);
+    }
   }
 }
 
-var oReq = new XMLHttpRequest();
-oReq.addEventListener('load', reqListener);
-oReq.open('GET', 'https://www.reddit.com/r/birdswitharms.json');
-oReq.send();
+function formatAuthenticator (url) {
+  let whiteList = ['.jpg', '.jpeg', '.gif', '.png', 'reddituploads'];
+  for (let i = 0; i < whiteList.length; i++){
+    if (url.indexOf(whiteList[i]) > -1){
+      return true;
+    }
+  }
+  return false;
+}
+
+function linkGenerator (){
+
+}
