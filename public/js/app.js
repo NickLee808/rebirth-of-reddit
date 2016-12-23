af674aa31a2d29f1ffed346c4e7f93ad5d6e5b57
Nick Lee <nicklee808@gmail.com>
2016-12-22 22:44:12 -1000

    header and navbar added

--- public/js/app.js
+++ public/js/app.js
@@ -22,9 +22,12 @@ function linkLoader () {
   let objParse = JSON.parse(this.responseText);
   let arrayOfPosts = objParse.data.children;
   for (let i = 0; i < arrayOfPosts.length; i++) {
-    let title= arrayOfPosts[i].data.title;
-    console.log(title);
-    if (formatAuthenticator(arrayOfPosts[i].data.url)) {
+    let title = arrayOfPosts[i].data.title;
+    let author = arrayOfPosts[i].data.author;
+    let comments = arrayOfPosts[i].data.num_comments;
+    let upvotes = arrayOfPosts[i].data.score;
+    let url = arrayOfPosts[i].data.url;
+    if (formatAuthenticator(url)) {
       let link = document.createElement('span');
       link.setAttribute('id', `link${i}`);
       document.getElementById('gallery').appendChild(link);
@@ -35,13 +38,17 @@ function linkLoader () {
         vid.setAttribute('width', '100%');
         vid.setAttribute('height', '100%');
         link.appendChild(vid);
+        titleGenerator(link, url, title);
+        subtitleGenerator (link, author, comments, upvotes);
       }else{
         let pic = document.createElement('img');
         pic.setAttribute('src', objParse.data.children[i].data.url.split('&amp;').join('&').split('gifv').join('gif'));
         link.appendChild(pic);
+        titleGenerator(link, url, title);
+        subtitleGenerator (link, author, comments, upvotes);
       }
     }else{
-      console.log('broken link: ' + arrayOfPosts[i].data.url);
+      console.log('unloadable link: ' + arrayOfPosts[i].data.url);
     }
   }
 }
@@ -64,6 +71,30 @@ function formatAuthenticator (url) {
   return false;
 }
 
-function titleGenerator (){
+function titleGenerator (link, url, title) {
+  let titleP = document.createElement('p');
+  titleP.setAttribute('id', 'title');
 
+  let titleLink = document.createElement('a');
+  titleLink.setAttribute('href', `${url}`);
+  titleLink.innerHTML = title;
+  titleP.appendChild(titleLink);
+
+  return link.appendChild(titleP);
+}
+
+function subtitleGenerator (link, author, comments, upvotes) {
+  let subtitle = document.createElement('p');
+  subtitle.setAttribute('id', 'subtitle');
+  subtitle.innerHTML= 'by ';
+  
+  let authorURL = 'https://www.reddit.com/user/' + `${author}`;
+  let authorLink = document.createElement('a');
+  authorLink.setAttribute('href', `${authorURL}`);
+  authorLink.innerHTML = `${author}`;
+  subtitle.appendChild(authorLink);
+  subtitle.appendChild(document.createElement('p'));
+
+/*subtitleP.innerHTML = 'by ' + authorLink + ' • ' + `${comments}` + ' comments' + ' • ' + `${upvotes}` + ' upvotes';
+*/  return link.appendChild(subtitle);
 }
